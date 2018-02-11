22af9f318c29905de6e1ebeb6f7cc079ff1fb503
Matthew Huffman <matt@matthewjhuffman.com>
2018-02-11 00:24:08 -0700

    Added struct to track bull and cow counts. Declared and began defining the SubmitGuess function.

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -12,6 +12,8 @@ void FBullCowGame::Reset()
 {
 	constexpr int32 MAX_TRIES = 8;
 	MyMaxTries = MAX_TRIES;
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
 	MyCurrentTry = 1;
 }
 
@@ -23,4 +25,17 @@ bool FBullCowGame::IsGameWon() const
 bool FBullCowGame::CheckGuessValidity(FString)
 {
 	return false;
+}
+
+// Receives a VALID guess, increments tries, and returns counts.
+FBullCowCount FBullCowGame::SubmitGuess(FString)
+{
+	// Increment the try number.
+	MyCurrentTry++;
+	// Setup a return variable.
+	FBullCowCount BullCowCount;
+	// Loop through all letters in the guess.
+		// Compare letters against the hidden word.
+
+	return BullCowCount;
 }
--- BullCowGame/FBullCowGame.h
+++ BullCowGame/FBullCowGame.h
@@ -4,6 +4,12 @@
 using FString = std::string;
 using int32 = int;
 
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
 	FBullCowGame(); // Constructor
@@ -13,11 +19,13 @@ public:
 	bool IsGameWon() const;
 	void Reset(); // TODO make a more rich return value.
 	bool CheckGuessValidity(FString); // TODO make a more rich return value.
-
-	// Provide a function for counting bulls and cows and incrementing try number.
+	
+									  // Count bulls and cows and incrementing try number, assuming valid guess.
+	FBullCowCount SubmitGuess(FString);
 
 private:
 	// See constructor for initialization.
 	int32 MyCurrentTry;
 	int32 MyMaxTries;
+	FString MyHiddenWord;
 };
